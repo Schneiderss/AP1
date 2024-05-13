fd05c33117e13f42ed5b788ed296ac48ff6b2e29
Schneiderss <167125378+Schneiderss@users.noreply.github.com>
2024-05-13 20:28:12 -0300

    Update conversor (m) para (cm) e (mm).c

--- conversor (m) para (cm) e (mm).c
+++ conversor (m) para (cm) e (mm).c
@@ -4,15 +4,15 @@
 
 int main(){
 	
-	setlocale(LC_ALL, "Portuguese");
+	setlocale(LC_ALL, "Portuguese");//Transforma ponto em vÃ­rgula
 	
 	float medida;
 	
 	printf("Converta uma medida de metros(m) em (cm) e (mm)!\n\n");
-	printf("Dê alguma medida em (m): ");
+	printf("DÃª alguma medida em (m): ");
 	scanf("%f", &medida);
 	
-	printf("\nA medida %.2fm, é %.2fcm", medida, medida*pow(10, 2));
-	printf("\nA medida %.2fm, é %.2fmm", medida, medida*pow(10, 3));
+	printf("\nA medida %.2fm, Ã© %.2fcm", medida, medida*pow(10, 2));
+	printf("\nA medida %.2fm, Ã© %.2fmm", medida, medida*pow(10, 3));
 	
 }
