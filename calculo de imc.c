08fe07026f7a28b5497aa79347143148379c737b
Schneiderss <167125378+Schneiderss@users.noreply.github.com>
2024-05-13 20:28:02 -0300

    Update calculo de imc.c

--- calculo de imc.c
+++ calculo de imc.c
@@ -3,20 +3,20 @@
 
 int main(){
 	
-	setlocale(LC_ALL, "Portuguese");
+	setlocale(LC_ALL, "Portuguese");//Transforma ponto em v√≠rgula
 	
 	float alt, peso, imc;
 	
-	printf("Descubra seu Õndice de Massa Corporal(IMC) \n\n");
+	printf("Descubra seu √çndice de Massa Corporal(IMC) \n\n");
 	
-	printf("Qual È o seu peso(em kg)? ");
+	printf("Qual √© o seu peso(em kg)? ");
 	scanf("%f", &peso);
 	
-	printf("Qual È a sua altura(em metros)? ");
+	printf("Qual √© a sua altura(em metros)? ");
 	scanf("%f", &alt);
 	
 	imc = peso / (alt*alt);
 	
-	printf("\nSeu IMC È: %.2f\n", imc);
+	printf("\nSeu IMC √©: %.2f\n", imc);
 	
 }
