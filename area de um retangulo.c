74f9e67cb28f41e4b0ebba70cd899aa4bf913105
Schneiderss <167125378+Schneiderss@users.noreply.github.com>
2024-05-13 20:27:40 -0300

    Update area de um retangulo.c

--- area de um retangulo.c
+++ area de um retangulo.c
@@ -3,17 +3,17 @@
 
 int main(){
 	
-	setlocale(LC_ALL, "Portuguese");
+	setlocale(LC_ALL, "Portuguese");//Transforma ponto em v√≠rgula
 	
 	float altura;
 	float comp;
 	
-	printf("Descubra a ·rea de um ret‚ngulo qualquer!\n\n");
+	printf("Descubra a √°rea de um ret√¢ngulo qualquer!\n\n");
 	printf("Digite uma altura: ");
 	scanf("%f", &altura);
 	printf("Digite um comprimento: ");
 	scanf("%f", &comp);
 	
-	printf("\nA ·rea desse ret‚ngulo È: %.2f", altura*comp);
+	printf("\nA √°rea desse ret√¢ngulo √©: %.2f", altura*comp);
 	
 }
